f6f1f884171dc11ad4400b935aa532fd8b7be2b2
Bill Melvin <bill@billmelvin.com>
2022-11-08 22:11:29 -0500

    Fill in UploadContext.

--- demo-webapp/demo-webapp/Data/UploadContext.cs
+++ demo-webapp/demo-webapp/Data/UploadContext.cs
@@ -1,6 +1,20 @@
-﻿namespace demo_webapp.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace demo_webapp.Data
 {
-    public class UploadContext
+    public class UploadContext : DbContext
     {
+        public DbSet<Upload> Uploads => Set<Upload>();
+
+        public string DbPath { get; set; }
+
+        public UploadContext()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            DbPath = Path.Join(path, "demo-webapp.sqlite");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data_Source={DbPath}");
     }
 }
