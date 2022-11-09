dcde81ebab37e6915f44f8961c9a0ec67460e849
Bill Melvin <bill@billmelvin.com>
2022-11-08 22:17:55 -0500

    Run add-migration Initial-migration.

---
+++ demo-webapp/demo-webapp/Migrations/20221109031557_Initial-migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace demowebapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Uploads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FileName = table.Column<string>(type: "TEXT", nullable: false),
+                    ContentType = table.Column<string>(type: "TEXT", nullable: true),
+                    WhenUploaded = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<int>(name: "User_Id", type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uploads", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Uploads");
+        }
+    }
+}
