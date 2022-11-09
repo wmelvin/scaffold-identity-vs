cb23ed2db58d2a127d52504b4b3af50334cb6aa7
Bill Melvin <bill@billmelvin.com>
2022-11-09 09:43:02 -0500

    Add initial controller and view.

---
+++ demo-webapp/demo-webapp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo_webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
