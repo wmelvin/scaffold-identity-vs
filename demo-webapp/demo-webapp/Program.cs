38de8b94294da69f609e1187e90440cbdb928aad
Bill Melvin <bill@billmelvin.com>
2022-11-09 21:30:26 -0500

    Scaffold Login and Registration with no existing auth.

--- demo-webapp/demo-webapp/Program.cs
+++ demo-webapp/demo-webapp/Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using demo_webapp.Data;
+using demo_webapp.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//var connectionString = builder.Configuration.GetConnectionString("AuthContextConnection") 
+//    ?? throw new InvalidOperationException("Connection string 'AuthContextConnection' not found.");
+
+var appdata_path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+string connectionString = $"Data Source={Path.Join(appdata_path, "demo-webapp-auth.sqlite")}";
+
+builder.Services.AddDbContext<AuthContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDefaultIdentity<AuthUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthContext>();
 
 builder.Services.AddControllersWithViews();
 
@@ -12,5 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
+app.UseAuthentication();;
 
 app.Run();
